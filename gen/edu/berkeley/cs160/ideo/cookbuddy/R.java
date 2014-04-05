a916554134e579986668e30d4c04c6b6b1459948
Justin MacMillin <justinmacmillin@gmail.com>
2014-04-05 16:55:37 -0700

    added pics

--- gen/edu/berkeley/cs160/ideo/cookbuddy/R.java
+++ gen/edu/berkeley/cs160/ideo/cookbuddy/R.java
@@ -21,6 +21,7 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
+<<<<<<< Updated upstream
         public static final int alarm=0x7f020000;
         public static final int back=0x7f020001;
         public static final int backrepeat=0x7f020002;
@@ -68,18 +69,57 @@ public final class R {
         public static final int recipe_page_1=0x7f030003;
         public static final int recipe_page_2=0x7f030004;
         public static final int recipe_page_3=0x7f030005;
+=======
+        public static final int add=0x7f020000;
+        public static final int back=0x7f020001;
+        public static final int backrepeat=0x7f020002;
+        public static final int circle=0x7f020003;
+        public static final int help=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int profile=0x7f020006;
+        public static final int rounded=0x7f020007;
+        public static final int shape=0x7f020008;
+        public static final int whitecircle=0x7f020009;
+    }
+    public static final class id {
+        public static final int action_settings=0x7f08000a;
+        public static final int addRecipeButton=0x7f080002;
+        public static final int helpPageButton=0x7f080004;
+        public static final int menu=0x7f080000;
+        public static final int profilePageButton=0x7f080003;
+        public static final int textView1=0x7f080001;
+        public static final int textViewAddRecipe=0x7f080006;
+        public static final int textViewHelp=0x7f080007;
+        public static final int textViewHelpText=0x7f080009;
+        public static final int textViewHelpTitle=0x7f080008;
+        public static final int textViewUser=0x7f080005;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int help=0x7f030001;
+>>>>>>> Stashed changes
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int Cook_Buddy=0x7f050004;
+<<<<<<< Updated upstream
         public static final int Voice_Commands=0x7f050005;
+=======
+        public static final int Voice_Commands=0x7f050007;
+>>>>>>> Stashed changes
         public static final int action_settings=0x7f050001;
+        public static final int add_recipe=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int help=0x7f050003;
+<<<<<<< Updated upstream
         public static final int help_text=0x7f050006;
+=======
+        public static final int help_text=0x7f050008;
+        public static final int profile=0x7f050005;
+>>>>>>> Stashed changes
     }
     public static final class style {
         /** 
