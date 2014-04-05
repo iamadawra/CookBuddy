b768f29d6e0bae3b3a43f9ed5235a4fbe9251b97
Prashan Dharmasena <prashan14@gmail.com>
2014-04-05 16:39:30 -0700

    davids shit

--- gen/edu/berkeley/cs160/ideo/cookbuddy/R.java
+++ gen/edu/berkeley/cs160/ideo/cookbuddy/R.java
@@ -21,31 +21,65 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int back=0x7f020000;
-        public static final int backrepeat=0x7f020001;
-        public static final int circle=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int shape=0x7f020004;
+        public static final int alarm=0x7f020000;
+        public static final int back=0x7f020001;
+        public static final int backrepeat=0x7f020002;
+        public static final int calling_icon=0x7f020003;
+        public static final int circle=0x7f020004;
+        public static final int help=0x7f020005;
+        public static final int home_icon=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int message_icon=0x7f020008;
+        public static final int new_recipe_icon_2=0x7f020009;
+        public static final int next_icon=0x7f02000a;
+        public static final int plus=0x7f02000b;
+        public static final int previous_icon=0x7f02000c;
+        public static final int profile=0x7f02000d;
+        public static final int shape=0x7f02000e;
+        public static final int user=0x7f02000f;
+        public static final int whitecircle=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080004;
+        public static final int Button_1=0x7f08000e;
+        public static final int Button_2=0x7f08000f;
+        public static final int Button_3=0x7f080010;
+        public static final int Button_4=0x7f080011;
+        public static final int Button_5=0x7f080012;
+        public static final int action_settings=0x7f080013;
+        public static final int addRecipeButton=0x7f080004;
+        public static final int helpPageButton=0x7f080002;
         public static final int menu=0x7f080000;
-        public static final int recipe_instruction=0x7f080003;
-        public static final int recipe_title=0x7f080002;
+        public static final int next_step=0x7f08000c;
+        public static final int previous_step=0x7f08000d;
+        public static final int profilePageButton=0x7f080003;
+        public static final int recipe_1=0x7f08000a;
+        public static final int recipe_instruction=0x7f080006;
+        public static final int recipe_page_step_1=0x7f08000b;
+        public static final int recipe_start=0x7f080007;
+        public static final int recipe_title=0x7f080005;
         public static final int textView1=0x7f080001;
+        public static final int textViewHelpText=0x7f080009;
+        public static final int textViewHelpTitle=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_recipelandingpage=0x7f030001;
+        public static final int help=0x7f030002;
+        public static final int recipe_page_1=0x7f030003;
+        public static final int recipe_page_2=0x7f030004;
+        public static final int recipe_page_3=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Cook_Buddy=0x7f050004;
+        public static final int Voice_Commands=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int help=0x7f050003;
+        public static final int help_text=0x7f050006;
     }
     public static final class style {
         /** 
