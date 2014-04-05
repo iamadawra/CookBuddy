3d47211a12f3c5381d074b72a7d1bebb1424c7d8
Justin MacMillin <justinmacmillin@gmail.com>
2014-04-05 14:27:59 -0700

    messing around with the fucking buttons on the front page

--- src/edu/berkeley/cs160/ideo/cookbuddy/MainActivity.java
+++ src/edu/berkeley/cs160/ideo/cookbuddy/MainActivity.java
@@ -2,10 +2,12 @@ package edu.berkeley.cs160.ideo.cookbuddy;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 	
@@ -23,6 +25,15 @@ public class MainActivity extends Activity {
 		menu = (ExpandableListView) findViewById(R.id.menu);
 		menu.setAdapter(new menuAdapter(this));
 		menu.expandGroup(0);
+		
+		// Set images for circle buttons at bottom
+		ImageButton helpButton = (ImageButton) findViewById(R.id.helpPageButton);
+		helpButton.setBackgroundResource(R.drawable.help);
+		ImageButton addRecipeButton = (ImageButton) findViewById(R.id.addRecipeButton);
+		addRecipeButton.setBackgroundResource(R.drawable.plus);
+		ImageButton profileButton = (ImageButton) findViewById(R.id.profilePageButton);
+		profileButton.setBackgroundResource(R.drawable.user);
+		//profileButton.setImageDrawable(Drawable.createFromPath("@drawable/profile"));
 	}
 
 	@Override
